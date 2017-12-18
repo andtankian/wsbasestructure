fb051de762ddfa34b0c9bcc77797a6536cfb38c8
Andrew Ribeiro <andtankian@live.com>
2017-12-18 16:37:58 -0200

    Refactored, ValidateAndMergeEntityCommand class that was very optimized and now it extends from AbstractCommand instead of implements ICommand directly Refactored, ValidateIDEntityCommand class that was also veru optimized now it extends from AbstractCommand instead of implements ICommand directly

--- src/java/br/com/wsbasestructure/rules/impl/ValidateAndMergeEntityCommand.java
+++ src/java/br/com/wsbasestructure/rules/impl/ValidateAndMergeEntityCommand.java
@@ -1,44 +1,37 @@
 package br.com.wsbasestructure.rules.impl;
 
+import br.com.wsbasestructure.rules.abstracts.AbstractCommand;
 import br.com.wsbasestructure.domain.abstracts.Entity;
 import br.com.wsbasestructure.dto.FlowContainer;
-import br.com.wsbasestructure.dto.Message;
-import br.com.wsbasestructure.dto.Result;
 import br.com.wsbasestructure.dto.interfaces.IHolder;
-import br.com.wsbasestructure.rules.interfaces.ICommand;
 import org.hibernate.Session;
 
 /**
  *
  * @author Andrew Ribeiro
  */
-public class ValidateAndMergeEntityCommand implements ICommand {
+public class ValidateAndMergeEntityCommand extends AbstractCommand {
+
+    public ValidateAndMergeEntityCommand() {
+        super();
+    }
 
     @Override
     public IHolder exe(IHolder holder, FlowContainer flowContainer) {
-        Entity e = (Entity)holder.getEntities().get(0);
-        Entity loaded = null;
-        Message m = new Message();
-        if (e.getId() == null || e.getId() == 0) {
-            flowContainer.getResult().setStatus(Result.ERROR);
-            m.setError("invalid id");
-            flowContainer.getFc().setMustContinue(false);
-            flowContainer.getResult().setMessage(m);
-        } else {
-            Session s = flowContainer.getSession();
-            loaded = (Entity) s.get(e.getClass(), e.getId());
-            if (loaded != null) {
+        super.exe(holder, flowContainer);
+        Session s = flowContainer.getSession();
+        Entity loaded = (Entity) s.get(e.getClass(), e.getId());
+        if (loaded != null) {
+            try {
                 loaded.merge(e);
-            } else {
-                m.setError("not found");
-                flowContainer.getResult().setStatus(Result.ERROR);
-                flowContainer.getResult().setMessage(m);
-                flowContainer.getFc().setMustContinue(false);
+            } catch (Exception ex) {
+                m.setError("merge error");
+                isValid = false;
             }
+        } else {
+            m.setError("not found");
+            isValid = false;
         }
-
-        holder.getEntities().set(0, loaded);
-        flowContainer.getResult().setHolder(holder);
 
         return holder;
     }
--- src/java/br/com/wsbasestructure/rules/impl/ValidateIDEntityCommand.java
+++ src/java/br/com/wsbasestructure/rules/impl/ValidateIDEntityCommand.java
@@ -1,30 +1,30 @@
 package br.com.wsbasestructure.rules.impl;
 
-import br.com.wsbasestructure.domain.abstracts.Entity;
 import br.com.wsbasestructure.dto.FlowContainer;
-import br.com.wsbasestructure.dto.Message;
-import br.com.wsbasestructure.dto.Result;
 import br.com.wsbasestructure.dto.interfaces.IHolder;
-import br.com.wsbasestructure.rules.interfaces.ICommand;
+import br.com.wsbasestructure.rules.abstracts.AbstractCommand;
 
 /**
  *
  * @author Andrew Ribeiro
  */
-public class ValidateIDEntityCommand implements ICommand{
+public class ValidateIDEntityCommand extends AbstractCommand{
+
+    public ValidateIDEntityCommand() {
+        super();
+    }    
 
     @Override
     public IHolder exe(IHolder holder, FlowContainer flowContainer) {
-        Entity e = (Entity)holder.getEntities().get(0);
-        Message m = new Message();
+
+        /*DON'T FORGET TO CALL SUPER!*/
+        super.exe(holder, flowContainer);
         
         Long id = e.getId();
         
         if(id == null || id == 0){
             m.setError("invalid id");
-            flowContainer.getResult().setMessage(m);
-            flowContainer.getResult().setStatus(Result.ERROR);
-            flowContainer.getFc().setMustContinue(false);
+            isValid = false;
         }
         
         return holder;
