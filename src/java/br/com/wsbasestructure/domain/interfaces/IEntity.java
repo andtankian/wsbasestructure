367bdb5652cde9d27acc2a78c860cf1ddef10134
Andrew Ribeiro <andtankian@live.com>
2017-12-18 16:30:38 -0200

    Added, IEntity interface to provide signatures of important methods to every entity, like merge and defaultAcceptedFields

---
+++ src/java/br/com/wsbasestructure/domain/interfaces/IEntity.java
@@ -0,0 +1,13 @@
+package br.com.wsbasestructure.domain.interfaces;
+
+import br.com.wsbasestructure.domain.abstracts.Entity;
+import java.util.List;
+
+/**
+ *
+ * @author andrew
+ */
+public interface IEntity {
+    public void merge(Entity e) throws Exception;
+    public List defaultAcceptedFields(List accepts);
+}
