7c8b2c27ab260547c248a9caa573e5bad6493fe9
Andrew Ribeiro <andtankian@live.com>
2017-12-22 02:55:08 -0200

    Improvement, AbstractViewHelper now loads before and after main rules by itself and there is no need to each concrete viewhelper call these methods. Improvement, AbstractViewHelper now prepare fields of any entity and take accepts and rejects parameters automatically.

--- src/java/br/com/wsbasestructure/view/abstracts/AbstractViewHelper.java
+++ src/java/br/com/wsbasestructure/view/abstracts/AbstractViewHelper.java
@@ -1,40 +1,47 @@
 package br.com.wsbasestructure.view.abstracts;
 
+import br.com.wsbasestructure.domain.abstracts.Entity;
 import br.com.wsbasestructure.dto.FlowContainer;
 import br.com.wsbasestructure.dto.Result;
 import br.com.wsbasestructure.dto.interfaces.IHolder;
-import br.com.wsbasestructure.rules.interfaces.ICommand;
 import br.com.wsbasestructure.view.impl.GenericExclusionStrategy;
 import br.com.wsbasestructure.view.interfaces.IViewHelper;
 import com.google.gson.FieldAttributes;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import javax.ws.rs.core.Response;
 
 /**
  *
  * @author Andrew Ribeiro
  */
-public abstract class AbstractViewHelper implements IViewHelper{
+public abstract class AbstractViewHelper implements IViewHelper {
 
     public AbstractViewHelper() {
-        rulesBeforeMainFlow = new ArrayList<>();
-        rulesAfterMainFlow = new ArrayList<>();
+        rulesBeforeMainFlow = new ArrayList();
+        rulesAfterMainFlow = new ArrayList();
+        rejects = new ArrayList();
+        accepts = new ArrayList();
     }
-     private List<ICommand> rulesBeforeMainFlow;
-     private List<ICommand> rulesAfterMainFlow;
-     protected String typeRequest;
-     protected List rejects;
+    private final List rulesBeforeMainFlow;
+    private final List rulesAfterMainFlow;
+    private Set ignore;
+    protected String typeRequest;
+    protected List rejects;
+    protected List accepts;
 
     @Override
     public IHolder getView(FlowContainer fc) {
         List l = fc.getCr().getUriInfo().getQueryParameters().get("treq");
-        typeRequest = l != null ? (String)l.get(0) : "crud";
+        typeRequest = l != null ? (String) l.get(0) : "crud";
+        loadBusinessRulesBeforeMainFlow();
+        loadBusinessRulesAfterMainFlow();
         return null;
     }
-     
-     
 
     @Override
     public String getTypeRequest() {
@@ -42,42 +49,37 @@ public abstract class AbstractViewHelper implements IViewHelper{
     }
 
     @Override
-    public List<ICommand> getRulesBeforeMainFlow() {
+    public List getRulesBeforeMainFlow() {
         return rulesBeforeMainFlow;
     }
 
     @Override
-    public List<ICommand> getRulesAfterMainFlow() {
+    public List getRulesAfterMainFlow() {
         return rulesAfterMainFlow;
-    }         
-
-    @Override
-    public void loadBusinessRulesBeforeMainFlow() {
     }
 
     @Override
-    public void loadBusinessRulesAfterMainFlow() {
-    }
-
-    
-    @Override
-    public String setView(Result result) {
+    public Response setView(Result result) {
         GsonBuilder gb = new GsonBuilder();
-
-        if (rejects != null && !rejects.isEmpty()) {
-            gb.addSerializationExclusionStrategy(new GenericExclusionStrategy() {
-                @Override
-                public boolean shouldSkipField(FieldAttributes fa) {
-                    return rejects.contains(fa.getName());
-                }
-            });
-        }
+        Entity e = (Entity) result.getHolder().getEntities().get(0);
+        prepareReturnFields(e.getAllFields(), e.getDefaultAcceptedFields());
+        gb.addSerializationExclusionStrategy(new GenericExclusionStrategy() {
+            @Override
+            public boolean shouldSkipField(FieldAttributes fa) {
+                return ignore.contains(fa.getName());
+            }
+        });
         Gson g = gb.create();
 
-        return g.toJson(result);
+        return Response.status(Response.Status.OK).entity(g.toJson(result)).build();
+    }
+    
+    private void prepareReturnFields(List all, List wannotations){
+        all.removeAll(wannotations);
+        all.removeAll(accepts);
+        all.retainAll(rejects);
+        
+        ignore = new HashSet(all);
     }
 
-  
-    
-     
 }
